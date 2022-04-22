28bec23343ad139b5df087d1df9ba62e575fb3d5
Aphelele Xwayi <aphelelexwayi482@gmail.com>
2022-04-22 15:34:06 +0200

    lay out

--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,15 @@
 function weeklyWage(band, weeklyHours) {
+Array
+for(let i= 0 ;1<band.length;1++){
+ 
+if (band.startsWith("A"){
+total += 23
+}else if(bland.startsWith("B")){
+tatal += 37
+}else if(bland.startsWith("B")){
+    tatal += 37
 
-};
+}else(unkon)
+}
+return error
+}
